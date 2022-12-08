0722953c0bd52ed7c7995ada90ff2d8a8c885db2
Danish Amjad Alvi <danishamjad@gmail.com>
2022-12-09 04:22:53 +0500

    Done with Diamond class. Will do Face subclass tomorrow.

--- Danish.py
+++ Danish.py
@@ -1,27 +1,25 @@
-import math
+
 import matplotlib.pyplot as plt
-
-n=5
-
-V = [ (x,y) for x in range(-n, n + 1) for y in range(-n, n + 1) if abs( x - (1/2)) + abs(y - 1/2)  <= 5  ]
-V_ = [(x,y) for x in range(-n,n + 1) for y in range(-n,n+1) if x + y == 6 and abs(x-y) <= 5 ]
-#print(V)
-X = [i for (i,j) in V]
-Y = [j for (i,j) in V]
-
-X_ = [i for (i,j) in V_]
-Y_ = [j for (i,j) in V_]
-
-plt.figure(figsize=(7, 7))
-#plt.plot([1,1], [2,3])
-#plt.fill([1,1,2,2],[1,2,2,1], facecolor='lightgrey')
-plt.scatter(X,Y, color = 'red')
-plt.scatter(X_,Y_, color = 'black')
-#plt.axis('off')
-plt.show()
-print(V)
-
 class Diamond :
-    def __init__(self, order):
-        self.order = order
-     V = [ (x,y) for x in range(-n, n + 1) for y in range(-n, n + 1) if abs( x - (1/2)) + abs(y - 1/2)  <= 5  ]
+    def __init__(self, n): # n is the order of the Aztec Diamond
+        self.n = n
+        self.D = [ [(x,y) for x in range(-n, n + 1) for y in range(-n - 1, n + 1) if x + y == i and abs(x - y) <= n ] for i in range(-n + 1, n + 2)   ] # D^{k}_{n} as defined on [1] p. 11
+
+    class Face :
+            # To be done tomorrow
+    def plot(self) :
+        D_blk = sum([ self.D[i] for i in range(0, 2*self.n + 1, 2 )], [])
+        D_red = sum([ self.D[i] for i in range(1, 2*self.n, 2 )], [])
+
+        X_red, Y_red = list(zip(*D_red))
+        X_blk, Y_blk = list(zip(*D_blk))
+
+        plt.figure(figsize=(7, 7))
+        plt.scatter(X_blk,Y_blk, color = 'black')
+        plt.scatter(X_red,Y_red, color = 'red')
+        plt.axis('off')
+        plt.show()
+
+
+grid = Diamond(4)
+grid.plot()
